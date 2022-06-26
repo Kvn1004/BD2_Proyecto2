c079f270689abc87afe45c9e95f60d9f9205b795
Kvn1004 <mugenfan_pextin@yahoo.es>
2022-06-26 13:01:33 -0600

    scripts imdb

---
+++ IMDB/create_tables.sql
@@ -0,0 +1,125 @@
+USE Master;
+
+CREATE DATABASE BD2;
+
+USE BD2;
+
+CREATE TABLE Category (
+    id      int NOT NULL IDENTITY PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE Profession (
+    id      int NOT NULL IDENTITY PRIMARY KEY,
+    name    VARCHAR(255)
+);
+
+CREATE TABLE TName (
+    id          int NOT NULL IDENTITY PRIMARY KEY,
+    primaryName VARCHAR(255) NOT NULL,
+    birthYear   int NOT NULL,
+    deathYear   int NOT NULL
+);
+
+CREATE TABLE NameProfession (
+    nameId          int FOREIGN KEY REFERENCES TName(id),
+    profressionID   int FOREIGN KEY REFERENCES Profession(id)
+);
+
+CREATE TABLE Principal (
+    id          int NOT NULL IDENTITY PRIMARY KEY,
+    nameId      int FOREIGN KEY REFERENCES TName(id),
+    titleId     int NOT NULL,
+    categoryId  int FOREIGN KEY REFERENCES Category(id),
+    jobId       int NOT NULL,
+    forder      VARCHAR(255) NOT NULL,
+    character   VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE TitleType (
+    id          int NOT NULL IDENTITY PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Genre (
+    id          int NOT NULL IDENTITY PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Title (
+    id              int NOT NULL IDENTITY PRIMARY KEY,
+    titleTypeId     int FOREIGN KEY REFERENCES TitleType(id),
+    primaryTitle    VARCHAR(255) NOT NULL,
+    originalTitle   VARCHAR(255) NOT NULL,
+    isAdult         INT NOT NULL,
+    startYear       INT NOT NULL,
+    endYear         INT NOT NULL,
+    runtime         INT NOT NULL
+);
+
+CREATE TABLE TitleGenre (
+    titleId     int FOREIGN KEY REFERENCES Title(id),
+    genreId     int FOREIGN KEY REFERENCES Genre(id)
+);
+
+CREATE TABLE KnowForTitle (
+    nameId          int FOREIGN KEY REFERENCES TName(id),
+    titleId         int FOREIGN KEY REFERENCES Title(id)
+);
+
+CREATE TABLE Director (
+    nameId          int FOREIGN KEY REFERENCES TName(id),
+    titleId         int FOREIGN KEY REFERENCES Title(id)
+);
+
+CREATE TABLE Writer (
+    nameId          int FOREIGN KEY REFERENCES TName(id),
+    titleId         int FOREIGN KEY REFERENCES Title(id)
+);
+
+CREATE TABLE Episode (
+    id              int NOT NULL IDENTITY PRIMARY KEY,
+    titleId         int FOREIGN KEY REFERENCES Title(id),
+    parentId        int NOT NULL,
+    season          int NOT NULL,
+    episode         int NOT NULL
+);
+
+CREATE TABLE Rating (
+    id              int NOT NULL IDENTITY PRIMARY KEY,
+    titleId         int FOREIGN KEY REFERENCES Title(id),
+    averageRating   float NOT NULL,
+    numVotes        int NOT NULL
+);
+
+CREATE TABLE Region (
+    id          int NOT NULL IDENTITY PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Language (
+    id          int NOT NULL IDENTITY PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE AlternativeType (
+    id          int NOT NULL IDENTITY PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE AlternativeAttribute (
+    id          int NOT NULL IDENTITY PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE AlternativeTitle (
+    id                      int NOT NULL IDENTITY PRIMARY KEY,
+    titleId                 int FOREIGN KEY REFERENCES Title(id),
+    regionId                int FOREIGN KEY REFERENCES Region(id),
+    languageId              int FOREIGN KEY REFERENCES Language(id),
+    alternativeTypeId       int FOREIGN KEY REFERENCES AlternativeType(id),
+    alternativeAttributeId  int FOREIGN KEY REFERENCES AlternativeAttribute(id),
+    title                   VARCHAR(255) NOT NULL,
+    ordering                INT NOT NULL,
+    isOriginal              INT NOT NULL
+);
---
+++ IMDB/data.sql
@@ -0,0 +1,37 @@
+
+INSERT INTO Category VALUES ('Action & Adventure');
+
+INSERT INTO Profession VALUES ('Scriptwriter');
+
+INSERT INTO TName VALUES ('Guillermo del Toro', 1964, -1);
+
+INSERT INTO NameProfession VALUES (1, 1);
+
+INSERT INTO TitleType VALUES ('Principal');
+
+INSERT INTO Title VALUES (1, 'The Shape of Water', 'The Shape of Water', 1, 2017, 2017, 123);
+
+INSERT INTO KnowForTitle VALUES (1, 1);
+
+INSERT INTO Director VALUES (1, 1);
+
+INSERT INTO Writer Values (1, 1);
+
+INSERT INTO Genre VALUES ('Action');
+
+INSERT INTO TitleGenre VALUES (1, 1);
+
+INSERT INTO Rating VALUES (1, 5.0, 2000);
+
+INSERT INTO Episode VALUES (1, 1, 1, 1);
+
+INSERT INTO Region VALUES ('USA');
+
+INSERT INTO Language VALUES ('Inglés');
+
+INSERT INTO AlternativeType VALUES ('Pelicula');
+
+INSERT INTO AlternativeAttribute VALUES ('Acción');
+
+INSERT INTO AlternativeTitle VALUES (1, 1, 1, 1, 1, 'La forma del agua', 100, 1);
+
